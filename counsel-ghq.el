c6c276431d123c2b353d7c2ecba1659e94313863
SuzuimyaAoba <SuzumiyaAoba@gmail.com>
2018-12-29 22:38:03 +0900

    Use ivy-thing-at-point instead of counsel-symbol-at-point

--- counsel-ghq.el
+++ counsel-ghq.el
@@ -142,7 +142,7 @@
   (interactive)
   (ivy-read (concat "ghq list [M-o to menu]: ") (counsel-ghq--list-candidates)
             :keymap counsel-describe-map
-            :preselect (counsel-symbol-at-point)
+            :preselect (ivy-thing-at-point)
             :history 'counsel-describe-symbol-history
             :require-match t
             :sort nil
